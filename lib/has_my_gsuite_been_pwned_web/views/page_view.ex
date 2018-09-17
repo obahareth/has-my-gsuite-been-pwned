75bd7419e2979f7cd8822c26488ddd662ec35717
Omar Bahareth <omarb.work@gmail.com>
2018-09-17 23:19:45 +0300

    Add view helper function to get user name or email

--- lib/has_my_gsuite_been_pwned_web/views/page_view.ex
+++ lib/has_my_gsuite_been_pwned_web/views/page_view.ex
@@ -1,3 +1,13 @@
 defmodule HasMyGsuiteBeenPwnedWeb.PageView do
   use HasMyGsuiteBeenPwnedWeb, :view
+
+  def user_name(conn) do
+    current_user = conn.assigns.current_user
+
+    if (String.length(current_user.name) > 0) do
+      current_user.name
+    else
+      current_user.email
+    end
+  end
 end
