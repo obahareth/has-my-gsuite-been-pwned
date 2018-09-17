43a060331006a4a2374b42ad55b72974152d8cd7
Omar Bahareth <omarb.work@gmail.com>
2018-09-17 23:20:34 +0300

    Setup OAuth routes and assign current user in conn

--- lib/has_my_gsuite_been_pwned_web/router.ex
+++ lib/has_my_gsuite_been_pwned_web/router.ex
@@ -7,6 +7,7 @@ defmodule HasMyGsuiteBeenPwnedWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :assign_current_user
   end
 
   pipeline :api do
@@ -17,10 +18,20 @@ defmodule HasMyGsuiteBeenPwnedWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/auth", AuthController, :index
+    get "/oauth-callback", AuthController, :callback
+    delete "/logout", AuthController, :delete
   end
 
   # Other scopes may use custom stacks.
   # scope "/api", HasMyGsuiteBeenPwnedWeb do
   #   pipe_through :api
   # end
+
+  # Fetch the current user from the session and add it to `conn.assigns`. This
+  # will allow you to have access to the current user in your views with
+  # `@current_user`.
+  defp assign_current_user(conn, _) do
+    assign(conn, :current_user, get_session(conn, :current_user))
+  end
 end
