5db58d13462e7d5ef6598c0e1107ca1b705f0103
Omar Bahareth <omarb.work@gmail.com>
2018-09-17 21:58:20 +0300

    Add OAuth2 dep

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule HasMyGsuiteBeenPwned.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:oauth2, "~> 0.9"}
     ]
   end
 
