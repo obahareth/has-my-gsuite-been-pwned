27dc8dc59c510fac046c17c058ef2514997017d9
Omar Bahareth <omarb.work@gmail.com>
2018-09-18 21:40:56 +0300

    Add wrapper for fetching G Suite directories

---
+++ lib/has_my_gsuite_been_pwned/google/directory.ex
@@ -0,0 +1,41 @@
+defmodule HasMyGsuiteBeenPwned.Google.Directory do
+  alias HasMyGsuiteBeenPwned.User
+
+  @directory_endpoint "https://www.googleapis.com/admin/directory/v1/users?customer=my_customer&maxResults=500"
+
+  def directory_endpoint(page_token) when is_nil(page_token), do: @directory_endpoint
+
+  def directory_endpoint(page_token) do
+    "#{@directory_endpoint}&pageToken=#{page_token}"
+  end
+
+  def fetch_directory!(oauth_client, page_token) do
+    endpoint = directory_endpoint(page_token)
+    %{body: directory} = OAuth2.Client.get!(oauth_client, endpoint)
+
+    directory
+  end
+
+  def fetch_entire_directory(oauth_client, page_token \\ nil, users \\ []) do
+    directory_response = fetch_directory!(oauth_client, page_token)
+
+    users_response = directory_response["users"]
+    next_page_token = directory_response["nextPageToken"]
+
+    next_users = users_response
+    |> flatten_users
+    |> Enum.concat(users)
+
+    if next_page_token do
+      fetch_entire_directory(oauth_client, next_page_token, next_users)
+    else
+      next_users
+    end
+  end
+
+  def flatten_users(users) do
+    Enum.map(users, fn u ->
+      %User{email: u["primaryEmail"], name: u["name"]["fullName"]}
+    end)
+  end
+end
