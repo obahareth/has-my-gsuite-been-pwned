d4192c7acecbb36b811d49d20dd95d9045024a4d
Omar Bahareth <omarb.work@gmail.com>
2018-09-21 19:54:31 +0300

    Add functions to check_user and avoid rate limit and simplified reports

--- lib/has_my_gsuite_been_pwned/wrappers/have_i_been_pwned.ex
+++ lib/has_my_gsuite_been_pwned/wrappers/have_i_been_pwned.ex
@@ -5,4 +5,35 @@ defmodule HasMyGsuiteBeenPwned.HaveIBeenPwned do
     user.email
     |> ExPwned.Breaches.breachedaccount
   end
+
+  def check_user_and_sleep(user = %User{}) do
+    case check_user(user) do
+      {:ok, breach_report, retry_after} ->
+        if (retry_after) do
+          # Sleep for retry_time + 150ms (to avoid getting hit by rate limit)
+          sleep_time = (retry_after[:retry_after] * 1000) + 150
+          :timer.sleep(sleep_time)
+        end
+
+        get_simplified_breach_report(breach_report, user)
+      _ -> nil
+    end
+  end
+
+  defp get_simplified_breach_report(report, user) do
+    IO.inspect(report)
+
+    if report == nil || report[:msg] do
+      nil
+    else
+      %{
+        "Name" => user.name,
+        "Email" => user.email,
+        "BreachDate" => report["BreachDate"],
+        "DataClasses" => report["DataClasses"] |> Enum.join(", "),
+        "Domain" => report["Domain"],
+        "Title" => report["Title"]
+      }
+    end
+  end
 end
